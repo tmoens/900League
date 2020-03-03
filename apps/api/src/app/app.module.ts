69908786f782e88f188b4a746d81af3ce33afa2b
Ted Moens <ted.moens@gmail.com>
2020-03-03 10:39:11 -0800

    Add proper server configuration.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -9,29 +9,47 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Player2teamModule } from './player2team/player2team.module';
 import { DataLoaderModule } from './data-loader/data-loader.module';
 import { Team2fixtureModule } from './team2fixture/team2fixture.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     TeamsModule,
     PlayersModule,
     FixturesModule,
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "900_league",
-      password: "900_league",
-      database: "900_league",
-      synchronize: true,
-      logging: ["error", "query"],
-      autoLoadEntities: true}),
     Player2teamModule,
     DataLoaderModule,
     Team2fixtureModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/api/.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        NODE_ENV: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        TYPEORM_LOGGING: Joi.string().required(),
+      })
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        logging: ["error"],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {
-
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,12 +6,14 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.port || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
